893147f2392e4cec0c212174f7eb3e727dad133e
Achmad Bakhtiar <123394200+bakhtiar2023@users.noreply.github.com>
2023-09-18 22:11:57 +0700

    controller test

---
+++ todo-app/__mocks__/mockData.js
@@ -0,0 +1,46 @@
+const mockTodoArray = [
+    {
+        "id": 3,
+        "title": "lorem",
+        "completed": true,
+        "createdAt": "2023-08-27T16:29:16.832Z",
+        "updatedAt": "2023-08-27T17:28:14.496Z"
+    },
+    {
+        "id": 4,
+        "title": "lorem",
+        "completed": false,
+        "createdAt": "2023-08-27T16:37:47.829Z",
+        "updatedAt": "2023-08-27T16:37:47.829Z"
+    },
+    {
+        "id": 5,
+        "title": "lorem",
+        "completed": false,
+        "createdAt": "2023-08-27T16:39:11.729Z",
+        "updatedAt": "2023-08-27T16:39:11.729Z"
+    },
+    {
+        "id": 6,
+        "title": "lorem",
+        "completed": false,
+        "createdAt": "2023-08-27T16:40:16.591Z",
+        "updatedAt": "2023-08-27T16:40:16.591Z"
+    },
+    {
+        "id": 7,
+        "title": "lorem",
+        "completed": false,
+        "createdAt": "2023-08-27T16:40:56.876Z",
+        "updatedAt": "2023-08-27T16:40:56.876Z"
+    },
+    {
+        "id": 8,
+        "title": "ipsum",
+        "completed": true,
+        "createdAt": "2023-08-27T17:29:09.393Z",
+        "updatedAt": "2023-08-27T17:29:38.238Z"
+    }
+]
+
+module.exports = mockTodoArray
