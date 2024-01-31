7b2a7ae22821321c00c59b0611614c7fcad13d9c
Achmad Bakhtiar <123394200+bakhtiar2023@users.noreply.github.com>
2024-02-01 01:56:56 +0700

    Update index.js

--- todo-app/db/models/index.js
+++ todo-app/db/models/index.js
@@ -14,7 +14,7 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], { ...config, dialectModule: pg }, { dialectOptions: { ssl: true } });
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, { ...config, dialectModule: pg }, { dialectOptions: { ssl: true } });
+  sequelize = new Sequelize(config.database, config.username, config.password, { ...config, dialectModule: 'postgresql' }, { dialectOptions: { ssl: true } });
 }
 
 fs
